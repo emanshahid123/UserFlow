0cb5c7d92cf9337a5d76f973404d7ad2ddfbf4c1
emanshahid123 <145204508+emanshahid123@users.noreply.github.com>
2025-07-14 15:34:28 +0500

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoWebApp.Model
+{
+    public class Users
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
